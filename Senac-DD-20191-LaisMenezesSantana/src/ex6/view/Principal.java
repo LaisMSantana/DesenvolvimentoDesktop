6ea7361b9d819fe7452ef6a2178b446e64ad8347
lais <lais.santana123@hotmail.com>
2019-06-30 16:03:55 -0300

    ex 6

--- Senac-DD-20191-LaisMenezesSantana/src/ex6/view/Principal.java
+++ Senac-DD-20191-LaisMenezesSantana/src/ex6/view/Principal.java
@@ -22,124 +22,6 @@ import javax.swing.JLabel;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-<<<<<<< HEAD
-
-public class Principal extends JFrame {
-
-	private JPanel contentPane;
-	private JDesktopPane desktopPane;
-	private JLabel lblSistemDeGerenciamento;
-	CadastroCliente cadastroCliente = null;
-	TelaSobre s = null;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Principal frame = new Principal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public Principal() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 690, 400);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JMenu mnClientes = new JMenu("Clientes");
-		mnClientes.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-fila.png")));
-		menuBar.add(mnClientes);
-		
-		JMenuItem mntmCadastrar = new JMenuItem("Cadastrar");
-		mntmCadastrar.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(cadastroCliente == null) {
-				cadastroCliente = new CadastroCliente();
-				desktopPane.add(cadastroCliente);
-				cadastroCliente.show();
-				} else if(cadastroCliente != null) {
-					cadastroCliente.setVisible(true);
-				}
-			}
-		});
-		
-		mntmCadastrar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_UNDEFINED, 0));
-		mntmCadastrar.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-adicionar-usuário-masculino.png")));
-		mnClientes.add(mntmCadastrar);
-		
-		JMenuItem mntmListar = new JMenuItem("Listar");
-		mntmListar.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-cardápio.png")));
-		mnClientes.add(mntmListar);
-		
-		JMenuItem mntmRelatrio = new JMenuItem("Relatório");
-		mntmRelatrio.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-documento-regular.png")));
-		mnClientes.add(mntmRelatrio);
-		
-		JMenu mnProdutos = new JMenu("Produtos");
-		mnProdutos.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-comprar.png")));
-		menuBar.add(mnProdutos);
-		
-		JMenu mnFuncionrios = new JMenu("Funcionários");
-		mnFuncionrios.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-treinamento.png")));
-		menuBar.add(mnFuncionrios);
-		
-		JMenu mnSobre = new JMenu("Sobre");
-		mnSobre.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-cartão-de-crédito-sem-contato.png")));
-		menuBar.add(mnSobre);
-		
-		JMenuItem mntmManual = new JMenuItem("Manual");
-		mntmManual.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F10, 0));
-		mntmManual.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-gerente-de-informações-do-cliente.png")));
-		mnSobre.add(mntmManual);
-		
-		JMenuItem mntmAjuda = new JMenuItem("Ajuda");
-		mntmAjuda.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F11, 0));
-		mntmAjuda.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-suporte-on-line-filled-50.png")));
-		mnSobre.add(mntmAjuda);
-		
-		JMenuItem mntmAutores = new JMenuItem("Autores");
-		mntmAutores.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(s == null) { 
-				s = new TelaSobre();
-				desktopPane.add(s);
-				s.setVisible(true);
-				} else if(s != null) {
-				s.setVisible(true);
-				}
-			}
-		});
-		mntmAutores.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F12, 0));
-		mntmAutores.setIcon(new ImageIcon(Principal.class.getResource("/icones/icons8-charlie-chaplin-64.png")));
-		mnSobre.add(mntmAutores);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JDesktopPane desktopPane = new JDesktopPane();
-		desktopPane.setBounds(0, -11, 690, 246);
-		contentPane.add(desktopPane);
-		
-		lblSistemDeGerenciamento = new JLabel("Sistem De Gerenciamento de Vendas");
-		lblSistemDeGerenciamento.setBounds(224, 257, 262, 15);
-		contentPane.add(lblSistemDeGerenciamento);
-	}
-=======
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 
@@ -175,6 +57,7 @@ public class Principal extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
 	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 877, 515);
@@ -328,6 +211,4 @@ public class Principal extends JFrame {
 		desktopPane_1.setBounds(0, 0, 851, 387);
 		contentPane.add(desktopPane_1);
 	}
-	
->>>>>>> branch 'master' of https://github.com/LaisMSantana/DesenvolvimentoDesktop.git
 }
